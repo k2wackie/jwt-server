a95296560c8a9e9eb318508a4bfde391dfdc7736
Kyle KIM <k2wackie@gmail.com>
2022-06-06 14:35:12 +0900

    Initial commit

---
+++ src/main/java/com/cos/jwt/config/SecurityConfig.java
@@ -0,0 +1,57 @@
+package com.cos.jwt.config;
+
+import com.cos.jwt.config.jwt.JwtAuthorizationFilter;
+import com.cos.jwt.repository.UserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.filter.CorsFilter;
+
+import com.cos.jwt.config.jwt.JwtAuthenticationFilter;
+import com.cos.jwt.filter.MyFilter1;
+import com.cos.jwt.filter.MyFilter3;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity // 시큐리티 활성화 -> 기본 스프링 필터체인에 등록
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	private final CorsFilter corsFilter;
+
+	private final UserRepository userRepository;
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+//			.addFilterBefore(new MyFilter3(), BasicAuthenticationFilter.class) //필터 순서 테스트
+			.csrf().disable()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) //세션을 사용하지 않겠다는..
+			.and()
+			.addFilter(corsFilter)  // @CrossOrigin(인증이없을때), 인증이 있을 때는 시큐리티필터에 등록해줘야 함
+			.formLogin().disable()
+			.httpBasic().disable()
+			.addFilter(new JwtAuthenticationFilter(authenticationManager()))  //AuthenticationManager던저줘야..
+			.addFilter(new JwtAuthorizationFilter(authenticationManager(), userRepository))
+			.authorizeRequests()
+			.antMatchers("/api/v1/user/**")
+			.access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+			.antMatchers("/api/v1/manager/**")
+			.access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+			.antMatchers("/api/v1/admin/**")
+			.access("hasRole('ROLE_ADMIN')")
+			.anyRequest().permitAll();
+	}
+
+}
---
+++ src/main/java/com/cos/jwt/controller/RestApiController.java
@@ -0,0 +1,51 @@
+package com.cos.jwt.controller;
+
+import com.cos.jwt.model.User;
+import com.cos.jwt.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class RestApiController {
+
+	private final UserRepository userRepository;
+
+	private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	@GetMapping("/home")
+	public String home() {
+		return "<h1>home</h1>";
+	}
+	
+	@PostMapping("/token")
+	public String token() {
+		return "<h1>token</h1>";
+	}
+
+	@PostMapping("/join")
+	public String join(@RequestBody User user) {
+		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+		user.setRoles("ROLE_USER");
+		userRepository.save(user);
+		return "회원가입완료";
+	}
+
+	@GetMapping("/user")
+	public String user() {
+		return "user";
+	}
+
+	@GetMapping("/manager")
+	public String manager() {
+		return "manager";
+	}
+
+	@GetMapping("/admin")
+	public String admin() {
+		return "admin";
+	}
+}
